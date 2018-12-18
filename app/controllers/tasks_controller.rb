33e897012df292cb77ec55fb60de283dd45b5e8a
yuusuke.fujino <wisteriafield_soccer7@yahoo.co.jp>
2018-12-18 13:28:57 +0000

    CRUD messages

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,26 @@
+class MessagesController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
